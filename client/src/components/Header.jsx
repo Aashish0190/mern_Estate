08ce30ca622046fbfb45ee15361249f743fc4a01
Aashish0190 <ashishkumar44521@gmail.com>
2024-09-12 03:40:56 +0530

    complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,29 @@
 import { FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+
 
 export default function Header() {
-  const { currentuser } = useSelector(state => state.user); // Correct spelling for currentUser
+  const { currentuser } = useSelector(state => state.user); 
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
 
   return (
     <header className="bg-gradient-to-r from-teal-500 via-blue-700 to-blue-900 shadow-lg">
@@ -15,13 +35,16 @@ export default function Header() {
           </Link>
         </h1>
 
-        <form className="bg-gray-800 p-2 rounded-full flex items-center shadow-inner w-full max-w-md mt-4 sm:mt-0 sm:max-w-xs lg:max-w-md">
+        <form onSubmit={handleSubmit}
+        className="bg-gray-800 p-2 rounded-full flex items-center shadow-inner w-full max-w-md mt-4 sm:mt-0 sm:max-w-xs lg:max-w-md">
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-full text-gray-300 px-3"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-indigo-400 ml-2 hover:text-indigo-500 transition duration-300" />
+          <button><FaSearch className="text-indigo-400 ml-2 hover:text-indigo-500 transition duration-300" /></button>
         </form>
 
         <ul className="flex gap-6 mt-4 sm:mt-0 items-center">

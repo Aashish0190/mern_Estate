9bc71ccfa2dc67ef0b7508d242a632edd4012d9e
Aashish0190 <ashishkumar44521@gmail.com>
2024-09-12 13:16:47 +0530

    chunkSize

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target : 'http://localhost:8000',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
         secure: false,
       },
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust chunk size limit (in kilobytes), here set to 1000kb (1MB)
+  },
   plugins: [react()],
-})
+});
